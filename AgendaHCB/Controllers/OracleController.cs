a67b21b2a9b8c5f7f6298dc51c9fe45830d1d123
Orlando J. Rojas Gonzalez <67527098+OrlandoJ15@users.noreply.github.com>
2025-10-08 09:03:51 -0600

    1
    1

---
+++ AgendaHCB/Controllers/OracleController.cs
@@ -0,0 +1,28 @@
+﻿using AgendaHCB.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgendaHCB.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OracleController : Controller
+    {
+        private readonly OracleService _oracleService;
+
+        public OracleController(OracleService oracleService)
+        {
+            _oracleService = oracleService;
+        }
+
+        [HttpGet("empleados")]
+        public async Task<IActionResult> GetEmpleados()
+        {
+            var query = "SELECT * FROM EMPLEADOS FETCH FIRST 10 ROWS ONLY";
+            var resultado = await _oracleService.EjecutarQueryAsync(query);
+            return Ok(resultado);
+        }
+    }
+
+}
+
+
