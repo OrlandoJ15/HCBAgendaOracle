368bac13327cbb537cbdc101646feac7d5013b72
Orlando J. Rojas Gonzalez <67527098+OrlandoJ15@users.noreply.github.com>
2025-10-15 08:55:41 -0600

    3
    3

--- DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
+++ DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cf3f36a6d4111936474f3ebc5f7d6e8d48ed5b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21a603fbd450ba9697a5acff5e6986b99c31913d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
